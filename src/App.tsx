4b7878a669a0a6ca892e74429f4aaa72f9f01da5
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-02-19 15:51:54 +0200

    ts change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-console.log(process.env.REACT_APP_TEST_MESSAGE, 'DEVELOP')
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kek it's working {process.env.REACT_APP_TEST_MESSAGE} ON DEVELOP????
+          Edit <code>src/App.tsx</code> and save to reload. !!!!!!!!!!!!!!!!
         </p>
         <a
           className="App-link"
