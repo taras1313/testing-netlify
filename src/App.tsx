186ad84947f8fa0484688c637f0f10111869f71a
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-02-19 21:12:31 +0200

    testing router issues fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import {Routes, Route, BrowserRouter, Link} from 'react-router-dom'
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import "./App.css";
 
+const Home = () => <div>home</div>;
 
-const Home = () => (
-    <div>home</div>
-)
-
-const About = () => (
-    <div>about</div>
-)
+const About = () => <div>about</div>;
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <header className="App-header">
-
-                    <img src={logo} alt="react logo"/>
-                    <Link to="/">home</Link>
-                    <br/>
-                    <Link to="/about">about</Link>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                    </Routes>
-
-                </header>
-            </div>
-        </BrowserRouter>
-
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="react logo" />
+          <img src="/logo512.png" alt="react logo" />
+          <Link to="/">home</Link>
+          <br />
+          <Link to="/about">about</Link>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
