54d7618fe44a4ec6a1a2420fc95a4f3f86f84d71
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-02-19 21:04:22 +0200

    testing router issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {Routes, Route, BrowserRouter, Link} from 'react-router-dom'
 import './App.css';
 
+
+const Home = () => (
+    <div>home</div>
+)
+
+const About = () => (
+    <div>about</div>
+)
+
 function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
 
-  console.log('test')
+                    <Link to="/">home</Link>
+                    <br/>
+                    <Link to="/about">about</Link>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. !!!!!!!!!!!!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                </header>
+            </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
