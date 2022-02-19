d8c13e2ccd13f650b6684939a6c7f68b4b08e12d
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-02-19 15:55:41 +0200

    with error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState<string>(1)
   return (
     <div className="App">
       <header className="App-header">
