c42c1407ea6ab8fbf9d276d5714445dfc54b0679
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-02-20 01:12:13 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import "./App.css";
+import { About } from "./components/About";
 
 const Home = () => <div>home</div>;
-
-const About = () => <div>about</div>;
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          {/*<img src={logo} alt="react logo" />*/}
-          {/*<img src="/logo512.png" alt="react logo" />*/}
           <Link to="/">home</Link>
           <br />
           <Link to="/about">about</Link>
---
+++ src/components/About.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+// TSC_COMPILE_ON_ERROR=true // try to keep smth with implicit any
+// ESLINT_NO_DEV_ERRORS=true // try to keep smth unused
+export const About = ({ asd }: { asd?: string }) => <div>about</div>;
+// export const About = ({ asd }: { asd?: string }) => <div>about</div>;
