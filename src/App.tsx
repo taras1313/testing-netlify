29be77f46c7306f7602ffce1753622ea32d6737d
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-02-19 15:59:38 +0200

    with error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState<string>(1)
+
+  console.log('test')
+
   return (
     <div className="App">
       <header className="App-header">
